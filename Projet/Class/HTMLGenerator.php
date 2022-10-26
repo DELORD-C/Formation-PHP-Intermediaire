0a442caf1d22d91b8a9b7dab876992f80be41cc8
Clément <clement.delord@epitech.eu>
2022-10-26 11:03:29 +0200

    bootstrap

--- Projet/Class/HTMLGenerator.php
+++ Projet/Class/HTMLGenerator.php
@@ -10,14 +10,22 @@ class HTMLGenerator {
             else {
                 $date = 'Inconnue';
             }
+            $film->overview = substr($film->overview, 0, 150) . "...";
             $filmshtml .= "
-                <tr>
-                    <td>$film->original_title</td>
-                    <td><img src='https://image.tmdb.org/t/p/w500$film->poster_path'></td>
-                    <td>$date</td>
-                    <td>$film->overview</td>
-                    <td><a href='details.php?id=$film->id'>Voir</a></td>
-                </tr>
+                <a href='details.php?id=$film->id' class='card mb-3' style='width: 540px; height: 265px;'>
+                    <div class='row g-0'>
+                        <div class='col-md-4'>
+                        <img style='width: 100%; height: 100%; object-fit: cover' src='https://image.tmdb.org/t/p/w500$film->poster_path' class='img-fluid rounded-start' alt='poster'>
+                        </div>
+                        <div class='col-md-8'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>$film->original_title</h5>
+                            <p class='card-text' style='text-overflow: ellipsis;'>$film->overview</p>
+                            <p class='card-text'><small class='text-muted'>$date</small></p>
+                        </div>
+                        </div>
+                    </div>
+                </a>
             ";
         }
         return $filmshtml;
@@ -31,30 +39,34 @@ class HTMLGenerator {
             $date = 'Inconnue';
         }
         return "
-        <tr>
-            <td>Titre</td>
-            <td>$film->original_title</td>
-        </tr>
-        <tr>
-            <td>Résumé</td>
-            <td>$film->overview</td>
-        </tr>
-        <tr>
-            <td>Image</td>
-            <td><img src='https://image.tmdb.org/t/p/w500$film->poster_path'></td>
-        </tr>
-        <tr>
-            <td>Note moyenne</td>
-            <td>$film->vote_average</td>
-        </tr>
-        <tr>
-            <td>Nombre de votes</td>
-            <td>$film->vote_count</td>
-        </tr>
-        <tr>
-            <td>Date de parution</td>
-            <td>$date</td>
-        </tr>
+        <div class='col-auto'>
+            <img class='w-100' src='https://image.tmdb.org/t/p/w500$film->poster_path'>
+        </div>
+        <div class='col'>
+            <table class='table'>
+                <tr>
+                    <td>Titre</td>
+                    <td>$film->original_title</td>
+                </tr>
+                <tr>
+                    <td>Résumé</td>
+                    <td>$film->overview</td>
+                </tr>
+                <tr>
+                    <td>Note moyenne</td>
+                    <td>$film->vote_average</td>
+                </tr>
+                <tr>
+                    <td>Nombre de votes</td>
+                    <td>$film->vote_count</td>
+                </tr>
+                <tr>
+                    <td>Date de parution</td>
+                    <td>$date</td>
+                </tr>
+            </table>
+        </div>
+        
         ";
     }
 }
